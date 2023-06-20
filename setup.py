df0e513cfdb4dc4f739924ff17b6860ac71668a6
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2023-06-21 10:05:26 +1200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Product_Information",
+    version="0.19",
+    packages=find_packages(),
+    install_requires=["pandas"],
+    package_data={"": ["*.csv"]},
+    include_package_data=True,
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
+)
