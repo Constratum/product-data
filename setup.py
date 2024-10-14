23adc34a9355f1bb199abd4ae699044ea8081b13
Matt Bishop <129570940+MattBishop81@users.noreply.github.com>
2024-10-14 12:23:00 +1000

    Update setup.py
    Changes to Lining Grids

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="product_data",
-    version="0.056",
+    version="0.057",
     packages=find_packages(),
     install_requires=["pandas"],
     package_data={"": ["*.csv"]},
